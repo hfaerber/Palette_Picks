ac2721c29626d7cee26a75e1e4f5b1a2c32f2924
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-04 12:26:29 -0700

    Add table specs in migration

--- .db/migrations/20200203174521_initial.js
+++ .db/migrations/20200203174521_initial.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('projects', function (table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.timestamps(true, true);
+    })
+    .createTable('palettes', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('color_one');
+      table.string('color_two');
+      table.string('color_three');
+      table.string('color-four');
+      table.string('color_five');
+      table.integer('projects_id').unsigned();
+      table.foreign('projects_id').references('projects.id');
+      table.timestamps(true, true);
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTable('projects')
+    .dropTable('palettes')
 };
