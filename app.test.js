8d11a8024c711796fb354c4f79018ab5b41203cd
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-04 16:50:33 -0700

    Add jest and supertest dependencies and write GET all projects test

--- app.test.js
+++ app.test.js
@@ -7,7 +7,19 @@ const database = require('knex')(configuration)
 
 describe('Server', () => {
 
+  beforeEach(async () => {
+    await database.seed.run();
+  });
 
+  describe('GET /api/v1/projects', () => {
+    it('should return a 200 and all projects', async () => {
+      const expectedProjects = await database('projects').select();
+      const response = await request(app).get('/api/v1/projects');
+      const projects = response.body;
 
+      expect(response.status).toBe(200);
+      expect(projects).toEqual(expectedProjects);
+    });
+  });
 
 })
