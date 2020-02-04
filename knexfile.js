ec8067609ffd533a085b643fdd8f9f1ce5ee8237
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-03 17:46:05 -0700

    Set up migrations configuration and create migration file

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/projects',
+    migrations: {
+      directory: '.db/migrations'
+    },
     useNullAsDefault: true
   },
 
