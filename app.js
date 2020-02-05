988a1b815e27ebd54bc815dfb4ee51ec14d22729
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-04 21:19:35 -0700

    Build endpoint for POST new palette

--- app.js
+++ app.js
@@ -29,11 +29,32 @@ app.get('/api/v1/palettes/:id', async (request, response) => {
     if (palettes.length) {
       response.status(200).json(palettes[0])
     } else {
-      response.status(404).json({error: `Could not find palette with an id of ${id}`})
+      response.status(404).json(
+        {error: `Could not find palette with an id of ${id}`})
     }
   } catch (error) {
     response.status(500).json({ error })
   }
-})
+});
+
+app.post('/api/v1/projects/:id/palettes', async (request, response) => {
+  const projects_id = request.params.id;
+  const palette = { ...request.body, projects_id: Number(projects_id) };
+
+  for (let requiredParameter of ['name', 'color_one', 'color_two',
+    'color_three', 'color_four', 'color_five', 'projects_id']) {
+    if (!palette.hasOwnProperty(requiredParameter)) {
+      return response
+        .status(422)
+        .send({ error: `The expected format is: { name: <String>, color_one: <String>, color_two: <String>, color_three: <String>, color_four: <String>, color_five: <String> }. You are missing a "${requiredParameter}" property.` })
+    }
+  }
+  try {
+    const id = await database('palettes').insert(palette, 'id');
+    response.status(201).json({ id: id[0] });
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
 
 module.exports = app;
