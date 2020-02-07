533d12a9de6b752cab6831541bebf5c7c7cfc82d
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-07 15:16:59 -0700

    Remove semicolon

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev';
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
